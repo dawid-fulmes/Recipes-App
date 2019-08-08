ecad9d1301076fbd89f679e74b410636010f78ff
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-08-08 09:00:26 +0200

    Add endpoints for getting recipes, add proxy in client package.json

--- routes/recipes.js
+++ routes/recipes.js
@@ -15,4 +15,25 @@ router.post("/add", (req, res) => {
     .catch(err => res.status(400).json(err));
 });
 
+//GET
+//Get all recipes
+router.get("/", (req, res) => {
+  Recipe.find((err, recipes) => {
+    res.json(recipes);
+  });
+});
+
+//GET
+//Get recipe by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Recipe.findById(id, (err, recipe) => {
+    if (err) {
+      res.status(404).send("data is not found");
+    } else {
+      res.status(200).json(recipe);
+    }
+  });
+});
+
 module.exports = router;
