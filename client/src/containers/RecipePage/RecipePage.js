831ae088cc4040216ea4cab526532c089914793f
Dawid Fulmes <fulmes.dawid@gmail.com>
2019-08-13 17:58:09 +0200

    Build basic structure of RecipePage

--- client/src/containers/RecipePage/RecipePage.js
+++ client/src/containers/RecipePage/RecipePage.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getRecipes } from "../../store/actions";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Spinner from "react-spinkit";
 
-const RecipePage = ({ match }) => {
-  return <div>Page of recipe #{match.params.id}</div>;
+const RecipePage = ({ match, recipes, loading, error, getRecipes }) => {
+  const [recipe] = recipes.filter(({ _id }) => _id === match.params.id);
+  useEffect(() => {
+    getRecipes();
+  }, [getRecipes]);
+
+  if (error) {
+    return <ErrorMessage againClick={getRecipes} />;
+  } else if (loading) {
+    return <Spinner name="three-bounce" color="black" fadeIn="none" />;
+  } else {
+    if (recipe) {
+      return <div>There is recipe!!!!</div>;
+    } else {
+      return <div>There is no recipe :(</div>;
+    }
+  }
 };
 
-export default RecipePage;
+const mapStateToProps = state => ({
+  recipes: state.recipes,
+  loading: state.loading,
+  error: state.error,
+});
+const mapDispatchToProps = dispatch => ({
+  getRecipes: () => dispatch(getRecipes()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RecipePage);
